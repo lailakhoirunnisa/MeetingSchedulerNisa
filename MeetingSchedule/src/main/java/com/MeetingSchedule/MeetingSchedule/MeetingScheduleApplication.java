0f645babf8e90cfdad7d28f33a7cd851f2ef0462
lailakhoirunnisa <lailakhoirunnisa9@gmail.com>
2020-08-08 23:57:12 +0700

    yhaa

---
+++ MeetingSchedule/src/main/java/com/MeetingSchedule/MeetingSchedule/MeetingScheduleApplication.java
@@ -0,0 +1,14 @@
+package com.MeetingSchedule.MeetingSchedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingScheduleApplication.class, args);
+                System.out.println("Application is running");
+	}
+
+}
---
+++ MeetingSchedule/src/test/java/com/MeetingSchedule/MeetingSchedule/MeetingScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MeetingSchedule.MeetingSchedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
